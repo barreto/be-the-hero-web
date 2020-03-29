410765e4e94c5a8a1b89de87cfe4b7e37a43de0e
Victor Barreto <v.barreto.and@gmail.com>
2020-03-28 22:34:39 -0300

    feat(routes): defining routes to switch between project pages

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Logon from "./pages/Logon";
+
+export default function Routes(params) {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="./" component={Logon} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
