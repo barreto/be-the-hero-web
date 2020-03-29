d90ec0589fdc6c9abaf7773c6e50ad33083209c8
Victor Barreto <v.barreto.and@gmail.com>
2020-03-28 22:44:30 -0300

    feat(routes and register): fix path routes and include register page

---
+++ src/pages/Register/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Register(params) {
+  return (
+    <div>
+      <h1>Register</h1>
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Logon from "./pages/Logon";
+import Register from "./pages/Register";
 
 export default function Routes(params) {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="./" exact component={Logon} />
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" exact component={Register} />
       </Switch>
     </BrowserRouter>
   );
